0e7a791523b84885d6bf06770bda363784d90adb
bibi1989 <bibiaremieye@gmail.com>
2020-06-11 23:24:13 +0100

    added loading

--- src/components/Gigs/Gig.tsx
+++ src/components/Gigs/Gig.tsx
@@ -6,21 +6,25 @@ import { GigContext } from "../../context/GigProvider";
 import { Spinner } from "react-bootstrap";
 
 const Gig = () => {
-  const { fetchGig, gigs } = useContext(GigContext);
+  const { fetchGig, gigs, loading } = useContext(GigContext);
   useEffect(() => {
     fetchGig();
   }, []);
   return (
     <Container>
       <Flex>
-        {gigs.length <= 0 ? (
-          <div>
-            {/* <Spinner animation='border' variant='info' /> */}
-            <h1 style={{ textAlign: "center" }}>No Gig found!!!</h1>
+        {loading && (
+          <div style={{ textAlign: "center" }}>
+            <Spinner animation='border' variant='info' />
           </div>
-        ) : (
-          gigs.map((array: any) => <GigList gig={array} />)
         )}
+        {gigs.length <= 0
+          ? !loading && (
+              <div>
+                <h1 style={{ textAlign: "center" }}>No Gig found!!!</h1>
+              </div>
+            )
+          : gigs.map((array: any) => <GigList gig={array} />)}
       </Flex>
     </Container>
   );
--- src/context/GigProvider.tsx
+++ src/context/GigProvider.tsx
@@ -8,10 +8,11 @@ const ADD_GIGS = "ADD_GIGS";
 const SEARCH_GIG_LOCATION = "SEARCH_GIG_LOCATION";
 const SEARCH_GIG_PROF = "SEARCH_GIG_PROF";
 const SEARCH_GIG_TECH = "SEARCH_GIG_TECH";
+const LOADING = "LOADING";
 
 const initialState = {
   gigs: [],
-  store: [],
+  loading: false,
 };
 
 interface IAction {
@@ -20,7 +21,7 @@ interface IAction {
 
 interface IState {
   gigs: any[];
-  store: any[];
+  loading: boolean;
 }
 
 const reducer = (state: IState, action: any) => {
@@ -50,6 +51,11 @@ const reducer = (state: IState, action: any) => {
         ...state,
         gigs: [...action.payload],
       };
+    case LOADING:
+      return {
+        ...state,
+        loading: action.payload,
+      };
 
     default:
       return state;
@@ -57,60 +63,84 @@ const reducer = (state: IState, action: any) => {
 };
 
 const URL = "https://gig-api.herokuapp.com/api/v1/gig";
+// const URL = "http://localhost:5005/api/v1/gig";
 
 export const GigProvider = ({ children }: any) => {
   const [state, dispatch] = useReducer(reducer, initialState);
 
   const searchGigLocation = async (text: string) => {
     try {
+      dispatch({ type: LOADING, payload: true });
       const response = await axios.get(
         `http://localhost:5005/api/v1/gig/search?location=${text}`
       );
+      dispatch({ type: LOADING, payload: false });
       dispatch({ type: SEARCH_GIG_LOCATION, payload: response.data.data });
     } catch (error) {
+      dispatch({ type: LOADING, payload: false });
       console.log(error.response);
     }
   };
   const searchGigProficiency = async (text: string) => {
     try {
+      dispatch({ type: LOADING, payload: true });
       const response = await axios.get(
         `http://localhost:5005/api/v1/gig/search?proficiency=${text}`
       );
+      dispatch({ type: LOADING, payload: false });
       dispatch({ type: SEARCH_GIG_PROF, payload: response.data.data });
     } catch (error) {
+      dispatch({ type: LOADING, payload: false });
       console.log(error.response);
     }
   };
   const searchGigTech = async (text: string) => {
+    dispatch({ type: LOADING, payload: true });
     try {
       const response = await axios.get(
         `http://localhost:5005/api/v1/gig/search?tech=${text}`
       );
+      dispatch({ type: LOADING, payload: false });
       dispatch({ type: SEARCH_GIG_TECH, payload: response.data.data });
     } catch (error) {
+      dispatch({ type: LOADING, payload: false });
       console.log(error.response);
     }
   };
 
   const fetchGig = async () => {
-    const response = await axios.get("http://localhost:5005/api/v1/gig");
-    dispatch({ type: FETCH_GIGS, payload: response.data.data });
+    try {
+      dispatch({ type: LOADING, payload: true });
+      const response = await axios.get(`${URL}`);
+      dispatch({ type: FETCH_GIGS, payload: response.data.data });
+      dispatch({ type: LOADING, payload: false });
+    } catch (error) {
+      dispatch({ type: LOADING, payload: false });
+      console.log(error.response);
+    }
   };
 
   const addGig = async (gig: any) => {
-    const response = await axios.post("http://localhost:5005/api/v1/gig", gig, {
-      headers: {
-        "Content-type": "application/json",
-      },
-    });
-    dispatch({ type: ADD_GIGS, payload: response.data.data });
-    dispatch({ type: SEARCH_GIG_PROF, payload: response.data.data });
+    try {
+      dispatch({ type: LOADING, payload: true });
+      const response = await axios.post(`${URL}`, gig, {
+        headers: {
+          "Content-type": "application/json",
+        },
+      });
+      dispatch({ type: LOADING, payload: false });
+      dispatch({ type: ADD_GIGS, payload: response.data.data });
+    } catch (error) {
+      dispatch({ type: LOADING, payload: false });
+      console.log(error.response);
+    }
   };
   return (
     <GigContext.Provider
       value={{
         fetchGig,
         gigs: state.gigs,
+        loading: state.loading,
         addGig,
         searchGigLocation,
         searchGigProficiency,
