a64aacb7d2347dccc2ae63caab200c01223c04a3
bibi1989 <bibiaremieye@gmail.com>
2020-06-12 21:56:32 +0100

    added login and register

--- src/components/UserGig/UserGig.tsx
+++ src/components/UserGig/UserGig.tsx
@@ -2,6 +2,7 @@ import React, { useContext, useEffect } from "react";
 import styled from "styled-components";
 import { GigContext } from "../../context/GigProvider";
 import List from "./List";
+import { Icon } from "semantic-ui-react";
 
 const UserGig = () => {
   const { fetchProfileGig, gig, update, delete_gig } = useContext(GigContext);
@@ -9,11 +10,18 @@ const UserGig = () => {
   useEffect(() => {
     fetchProfileGig();
   }, [update, delete_gig]);
+
   return (
     <Container>
       <H1>Your Profile</H1>
       <Grid>
-        {gig !== undefined && gig.map((g: any) => <List key={g.id} gig={g} />)}
+        {gig.length <= 0 ? (
+          <H1>
+            <Icon name='edit' /> You have not added a profile
+          </H1>
+        ) : (
+          gig.map((g: any) => <List key={g.id} gig={g} />)
+        )}
       </Grid>
     </Container>
   );
@@ -29,4 +37,8 @@ const Container = styled.div`
   }
 `;
 const Grid = styled.div``;
-const H1 = styled.h1``;
+const H1 = styled.h1`
+  color: #555;
+  text-align: center;
+  padding-bottom: 2em;
+`;
